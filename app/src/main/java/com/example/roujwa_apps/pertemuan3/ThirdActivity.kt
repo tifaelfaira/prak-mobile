c1a8b92c5489520a07c3f6d99982a7813a564776
tifaelfaira <roujwa24si@mahasiswa.pcr.ac.id>
2026-03-30 10:29:06 +0700

    pertemuan 3

---
+++ app/src/main/java/com/example/roujwa_apps/pertemuan3/ThirdActivity.kt
@@ -0,0 +1,37 @@
+package com.example.roujwa_apps.pertemuan3
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.roujwa_apps.R
+import com.example.roujwa_apps.databinding.ActivityThirdBinding
+
+class ThirdActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityThirdBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityThirdBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val btnKirim : Button = findViewById(R.id.btnKirim)
+        val noTujuan : EditText = findViewById(R.id.inputNoTujuan)
+
+        binding.btnKirim.setOnClickListener {
+            val nomor = noTujuan.text
+            Toast.makeText(this, "Pesan Berhasil dikirim ke $nomor", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, ThirdResultActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
