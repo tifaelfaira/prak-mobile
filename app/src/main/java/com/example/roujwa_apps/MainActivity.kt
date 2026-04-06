3cd5f9d91ff18da0225fa6c10baf8e6b0b562ea9
tifaelfaira <roujwa24si@mahasiswa.pcr.ac.id>
2026-04-06 10:32:48 +0700

    pertemuan 4

--- app/src/main/java/com/example/roujwa_apps/MainActivity.kt
+++ app/src/main/java/com/example/roujwa_apps/MainActivity.kt
@@ -1,20 +1,36 @@
 package com.example.roujwa_apps
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.roujwa_apps.databinding.ActivityMainBinding
+import com.example.roujwa_apps.pertemuan_4.FourthActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        binding.btnPertemuan4.setOnClickListener {
+
+            val intent = Intent(this, FourthActivity::class.java)
+            intent.putExtra("nama", "Politeknik Caltex Riau")
+            intent.putExtra("asal", "Rumbai")
+            intent.putExtra("usia", 25)
+
+            startActivity(intent)
         }
     }
 }
---
+++ app/src/main/java/com/example/roujwa_apps/pertemuan_4/FourthActivity.kt
@@ -0,0 +1,47 @@
+package com.example.roujwa_apps.pertemuan_4
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.roujwa_apps.R
+import com.example.roujwa_apps.databinding.ActivityFourthBinding
+import com.google.android.material.snackbar.Snackbar
+
+class FourthActivity : AppCompatActivity() {
+    lateinit var binding: ActivityFourthBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityFourthBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_fourth)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val nama = intent.getStringExtra("nama")
+        val asal = intent.getStringExtra("asal")
+        val umur = intent.getIntExtra("umur",0)
+
+        Log.i("Data Intent","Nama: $nama , Usia: $umur, Asal: $asal")
+
+        binding.btnShowSnackbar.setOnClickListener {
+            Snackbar.make(binding.root, "Ini adalah Snackbar", Snackbar.LENGTH_SHORT)
+                .setAction("Tutup"){
+                    Log.e("Info Snackbar","Snackbar ditutup")
+                }
+                .show()
+        }
+
+        binding.btnShowAlertDialog.setOnClickListener {
+
+        }
+        }
+
+        }
+
