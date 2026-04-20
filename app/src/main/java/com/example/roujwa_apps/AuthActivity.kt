dfca4662225bebe07bc59cf2d6eca37c149b378f
tifaelfaira <roujwa24si@mahasiswa.pcr.ac.id>
2026-04-20 10:20:38 +0700

    pertemuan 6

---
+++ app/src/main/java/com/example/roujwa_apps/AuthActivity.kt
@@ -0,0 +1,54 @@
+package com.example.roujwa_apps
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.edit
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.roujwa_apps.databinding.ActivityAuthBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+
+class AuthActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAuthBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityAuthBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        //Kode ini harus selalu dipanggil saat butuh akses "user_pref"
+        val sharedPref = getSharedPreferences("user_pref", MODE_PRIVATE)
+
+        binding.btnLogin.setOnClickListener {
+            val email = binding.email.text.toString()
+            val password = binding.password.text.toString()
+            if (email == password && email.isNotEmpty() && password.isNotEmpty()) {
+
+                sharedPref.edit {
+                    putBoolean("isLogin", true)
+                    putString("email", email)
+                }
+
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            } else {
+                MaterialAlertDialogBuilder(this)
+                    .setTitle("Ooopss...")
+                    .setMessage("Email atau password salah! ")
+                    .setPositiveButton("close") { dialog, _ -> dialog.dismiss() }
+                    .show()
+            }
+        }
+
+
+    }
+
+
+}
--- app/src/main/java/com/example/roujwa_apps/MainActivity.kt
+++ app/src/main/java/com/example/roujwa_apps/MainActivity.kt
@@ -2,35 +2,67 @@ package com.example.roujwa_apps
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.edit
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.roujwa_apps.databinding.ActivityMainBinding
 import com.example.roujwa_apps.pertemuan_4.FourthActivity
+import com.example.roujwa_apps.pertemuan_5.FifthActivity // Import ini
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        binding.btnPertemuan4.setOnClickListener {
 
+        val sharedPref = getSharedPreferences("user_pref", MODE_PRIVATE)
+
+        // Tombol ke Pertemuan 4
+        binding.btnToFourth.setOnClickListener {
             val intent = Intent(this, FourthActivity::class.java)
             intent.putExtra("nama", "Politeknik Caltex Riau")
             intent.putExtra("asal", "Rumbai")
-            intent.putExtra("usia", 25)
-
+            intent.putExtra("umur", 25)
             startActivity(intent)
+        }
+
+        // ---  INI UNTUK PERTEMUAN 5 ---
+        binding.btnToFifth.setOnClickListener {
+            val intent = Intent(this, FifthActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        binding.btnLogout.setOnClickListener {
+            MaterialAlertDialogBuilder(this)
+                .setTitle("Konfirmasi")
+                .setMessage("Apakah Anda yakin ingin keluar?")
+                .setPositiveButton("Ya") { dialog, _ ->
+                    dialog.dismiss()
+                    sharedPref.edit{
+                        clear()
+                        apply()
+                    }
+                    finish()
+                }
+                .setNegativeButton("Batal") { dialog, _ ->
+                    dialog.dismiss()
+                    Log.e("Info Dialog","Anda memilih Tidak!")
+                }
+                .show()
         }
     }
 }
