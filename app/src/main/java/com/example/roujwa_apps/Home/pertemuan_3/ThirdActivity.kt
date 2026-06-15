ce4c54d4ba3fed825976ee32239749b869e13e54
tifaelfaira <roujwa24si@mahasiswa.pcr.ac.id>
2026-06-15 08:23:16 +0700

    "p13 Reminder notification "

--- app/src/main/java/com/example/roujwa_apps/Home/pertemuan_3/ThirdActivity.kt
+++ app/src/main/java/com/example/roujwa_apps/Home/pertemuan_3/ThirdActivity.kt
@@ -13,6 +13,8 @@ import com.example.roujwa_apps.R
 import com.example.roujwa_apps.databinding.ActivityThirdBinding
 import com.example.roujwa_apps.utils.NotificationHelper
 import com.example.roujwa_apps.utils.PermissionHelper
+import com.example.roujwa_apps.utils.ReminderHelper
+import java.util.Calendar
 
 class ThirdActivity : AppCompatActivity() {
     private lateinit var binding : ActivityThirdBinding
@@ -55,12 +57,26 @@ class ThirdActivity : AppCompatActivity() {
             val intent = Intent(this, ThirdResultActivity::class.java)
             //startActivity(intent)
 
-            NotificationHelper.showNotification(
-                this,
-                "Pesanan Anda",
-                "Halo $nomor, Pesanan Anda Sedang Diproses",
-                intent
+            // NotificationHelper.showNotification(
+            //    this,
+            //    "Pesanan Anda",
+            //    "Halo $nomor, Pesanan Anda Sedang Diproses",
+            //   intent
+          //  )
+
+            val calendar = Calendar.getInstance().apply {
+                add(Calendar.MINUTE, 1) // Tambah 1 menit dari sekarang
+            }
+
+            ReminderHelper.setReminder(
+                context = this, //Jika panggil di fragment maka requireContext()
+                hour = calendar.get(Calendar.HOUR_OF_DAY),
+                minute = calendar.get(Calendar.MINUTE),
+                title = "Reminder 1 Menit",
+                message = "Halo $nomor, reminder ini muncul 1 menit setelah tombol ditekan",
+                targetActivity = ThirdResultActivity::class.java
             )
+            Toast.makeText(this, "Silahkan tunggu 1 Menit untuk menerima Notifikasi...", Toast.LENGTH_SHORT).show()
+        }
         }
     }
-}
